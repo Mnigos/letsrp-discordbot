0a927de1ced71ed58a4e0ca6a9c9921607538ad1
MoneyIgos <makowskiigor@gmail.com>
2020-11-25 18:33:02 +0100

    Add cogs loader

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 
 from dotenv import load_dotenv, find_dotenv
-from os import getenv
+from os import getenv, listdir
 from discord.ext import commands
 
 
@@ -13,6 +13,14 @@ client = commands.Bot(command_prefix = 'rp!')
 @client.event
 async def on_ready():
     print('Bot is active!')
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
 
 
 client.run(key)
