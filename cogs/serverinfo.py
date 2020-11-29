d986ee9eb3eb481609c28b26fc32644ffab50de1
MoneyIgos <makowskiigor@gmail.com>
2020-11-29 13:49:25 +0100

    Add serverinfo command

---
+++ cogs/serverinfo.py
@@ -0,0 +1,30 @@
+import discord
+
+from discord.ext import commands
+from fivem import FiveM
+
+server = FiveM(ip = 'wyspa.letsrp.pl', port = 30120)
+
+
+class Serverinfo(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def serverinfo(self, ctx):
+        await ctx.message.delete()
+        info = await server.get_server_info()
+
+        embed = discord.Embed(
+            title = info.hostname,
+            colour = 0x62ff00
+        )
+        embed.add_field(name = 'IP Serwera', value = 'wyspa.letsrp.pl')
+        embed.add_field(name = 'Graczy', value = f'{info.clients}/{info.max_clients}')
+
+        await ctx.channel.send(embed = embed)
+
+
+def setup(client):
+    client.add_cog(Serverinfo(client))
